5949ba52444c151f6c0b99bbb68a9bc8e8ba59ee
louis <louis@youban.org>
2019-04-08 11:57:20 +0800

    test

--- inc/socketevent.h
+++ inc/socketevent.h
@@ -11,13 +11,23 @@ class socketEvent
         bool instance();
         bool epollInit();
         bool createListener();
+        void setSocketOpt();
+        void epollCreate();
+        void epollAddListener(int listenfd_);
+        void run();
+        void processConnectedFD(int fd_);
+        void processListenReq();
 
     private:
-        int epollFd;
+        
         int listenFd;
         int listenPort;
         char listenAddr[32];
-        int connFd;
+        //int connFd;
+
+        int epollFd;
+        struct epoll_event ev;
+        struct epoll_event events[2048];
 
 };
 
--- src/socketevent.cc
+++ src/socketevent.cc
@@ -3,12 +3,13 @@
 #include<string.h>    //memset
 #include<arpa/inet.h> //htons
 #include <netinet/in.h>  //地址格式
+#include <sys/epoll.h>
+#include<stdio.h>
 #include "socketevent.h"
-
 
 #define _MAX_FDSIZE_    2048
 #define _LISTEN_PORT_   11111
-
+#define _MAX_EPOLL_EVENT_ 2048
 
 namespace dzp{
 socketEvent::socketEvent():listenFd(-1),epollFd(-1)
@@ -32,21 +33,110 @@ bool socketEvent::instance()
 bool socketEvent::createListener()
 {
     struct sockaddr_in addr;
-    memset((char *) &addr, 0, sizeof(addr));
+    memset( (char *) &addr, 0, sizeof(addr) );
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(listenPort);
-    addr.sin_addr.s_addr = inet_addr(listenAddr);
+    addr.sin_port = htons( listenPort );
+    addr.sin_addr.s_addr = inet_addr( listenAddr );
 
-    listenFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    bind(listenFd, (struct sockaddr *)&addr, sizeof(addr));
-    listen(listenFd,5);
+    listenFd = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+    if( listenFd < 0 )
+    {
+        printf("create socket failed");
+        exit(-1);
+    }
 
-    socklen_t clilen;
-    struct sockaddr_in cliendAddr;
-    connFd = accept(listenFd,(struct sockaddr *)cliendAddr,&clilen);
-    //EINTER accept被信号中断
+    int rc = bind( listenFd, (struct sockaddr *)&addr, sizeof(addr) )
+    if( rc < 0 )
+    {
+        printf("bind socket failed");
+        exit(-1);
+    }
+
+    rc = listen( listenFd, 5 );
+    if (rc < 0 )
+    {
+        printf("listen failed");
+        exit(-1);
+    }
+    return true;
+}
+void socketEvent::setSocketOpt()
+{
+
 }
 
+void socketEvent::epollCreate()
+{
+    epollFd = epoll_create( _MAX_EPOLL_EVENT_ );
+    if( epollFd < 0 )
+    {
+        perror("epoll create failed");
+        exit(-1);
+    }
+}
+void socketEvent::epollAddListener( int listenfd_ )
+{
+    ev.enents = EPOLLIN;
+    ev.data.fd = listenfd_;
+    if( epoll_ctl( epollFd, EPOLL_CTL_ADD, listenfd_, &ev ) == -1 )
+    {
+        perror( "epoll_ctl: listen_sock" );
+        exit(-1);
+    }
+}
 
+void socketEvent::run()
+{
+    if( epollFd == -1 )
+    {
+        printf("run failed:epollFd=-1");
+    }
+    int nfds = -1;
+    nfds = epoll_wait( epollFd, events, _MAX_EPOLL_EVENT_, -1 );
 
+    if( nfds == -1 )
+    {
+        perror("epoll wait nfds = -1");
+        return -1;
+    }
+    /*else if( nfds == 0 )
+    {
+        //TODO::timeout
+    }
+    */
+    else
+    {
+        for( int i = 0; i < nfds ; ++i )
+        {
+            if( events[i].data.fd == listenFd )
+            {       
+                processListenReq();
+            }
+            else
+            {
+                processConnectedFD(events[i].data.fd);
+            }
+            
+        }
+    }
+}
+void socketEvent::processListenReq()
+{
+    int connFd_;
+    socklen_t clilen_;
+    struct sockaddr_in cliendAddr_;
+    connFd_ = accept( listenFd, (struct sockaddr *)cliendAddr_, &clilen_ );
+    if( connFd_ == -1 )
+        return;
+    ev.events == EPOLLIN;
+    ev.fd.data == connFd_;
+    if( epoll_ctl( epollFd, EPOLL_CTL_ADD, connFd_, &ev) == -1 )
+    {
+        perror( "epoll_ctl: conn_sock" );
+        exit(-1);
+    }
+}
+void socketEvent::processConnectedFD( int fd_)
+{
+    
 }
